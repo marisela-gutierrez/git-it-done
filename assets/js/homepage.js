0c315583e2870408885474660637a434f4c080b7
marigut <mari.gutierrez96@yahoo.com>
2021-04-28 00:37:21 -0500

    network error/no respo found alert

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,16 +2,26 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+
 var getUserRepos = function(user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   // make a request to the url
   fetch(apiUrl).then(function(response) {
+  if (response.ok) {
     response.json().then(function(data) {
       displayRepos(data, user);
     });
+  } else {
+    alert("Error: GitHub User Not Found");
+  }
+  })
+  .catch(function(error) {
+    // Notice this `.catch()` getting chained onto the end of the `.then()` method
+    alert("Unable to connect to GitHub");
   });
+
 };
 
 var formSubmitHandler = function(event) {
@@ -29,6 +39,12 @@ var formSubmitHandler = function(event) {
 };
 
 var displayRepos = function(repos, searchTerm) {
+
+  // check if api returned any repos
+  if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
   // clear old content
   repoContainerEl.textContent = "";
   repoSearchTerm.textContent = searchTerm;
